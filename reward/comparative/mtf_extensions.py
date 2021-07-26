4e14b16b01cecce582f4d9d232ab91700d165009
JFCeron <juanfceron@gmail.com>
2021-07-26 12:46:18 -0500

    Adapt Unitransformer for side-by-side target inputs

--- reward/comparative/mtf_extensions.py
+++ reward/comparative/mtf_extensions.py
@@ -1,9 +1,12 @@
+from copy import deepcopy
+
 import gin
 import tensorflow.compat.v1 as tf
 import mesh_tensorflow as mtf
 from mesh_tensorflow.transformer.transformer import \
     Unitransformer, Bitransformer, get_vocab_embedding_cls, make_layer_stack
 
+from reward.comparative.data import SEQUENCE_LENGTH
 from reward.comparative.loss_fn import comparative_paired_rewards_loss
 
 def get_dims_by_name(tensor, dim_name):
@@ -21,93 +24,125 @@ class ScalarOutputUnitransformer(Unitransformer):
         Overrrides Unitransformer._call_internal
         Adds losses to context!
         """
-        mesh = inputs.mesh
-        if self.ensemble_dim and self.ensemble_dim not in inputs.shape.dims:
-            # Training an ensemble where all models are trained on the same examples.
-            inputs = mtf.broadcast(inputs, [self.ensemble_dim] + inputs.shape.dims)
-            if targets:
-                targets = mtf.broadcast(
-                    targets, [self.ensemble_dim] + targets.shape.dims)
-        if "embedding" in context.shared_params:
-            vocab_embedding = context.shared_params["embedding"]
-        else:
-            vocab_embedding = get_vocab_embedding_cls()(
-                mesh,
-                self.input_vocab_dim,
-                self.model_dim,
-                context.variable_dtype,
-                name="embedding",
-                ensemble_dim=self.ensemble_dim)
-        x = vocab_embedding.ids_to_embedding(inputs)
-        if self.positional_embedding:
-            if "positional_embedding" in context.shared_params:
-                pos_emb_var = context.shared_params["positional_embedding"]
-            else:
-                pos_emb_var = mtf.layers.embedding_weights(
-                    mesh, self.max_length_dim, self.model_dim, context.variable_dtype,
-                    "positional_embedding", ensemble_dim=self.ensemble_dim)
-            if (context.length_dim is not None and
-                context.length_dim.size > self.max_length_dim.size):
-                message = (
-                    "Length dimenison exceeds size of positional embedding table. "
-                    "length_dim.size > max_length_dim.size %s vs %s."
-                    % (context.length_dim, self.max_length_dim))
-                if context.position_is_default:
-                    # Definitely getting overflow in this case.
-                    raise ValueError(message)
-                else:
-                    tf.logging.warning(
-                        message +
-                        " This may be OK if there are several shorter sequences packed "
-                        "together.  Otherwise, the later positions will get zeros.")
-            if context.position_is_default:
-                pos_emb = mtf.rename_dimension(
-                    mtf.slice(pos_emb_var, 0, context.length_dim.size,
-                            self.max_length_dim.name),
-                    self.max_length_dim.name, context.length_dim.name)
-            else:
-                pos_emb = mtf.gather(
-                    pos_emb_var, context.position, self.max_length_dim,
-                    output_shape=x.shape)
-            x += pos_emb
-        x = self.layer_stack.call(context, x)                                                                                                                                                                                                                                                                                        
-        rewards = mtf.layers.dense(
-            x,
-            new_dims=self._reward_dim,
-            reduced_dims=get_dims_by_name(x, "d_model"),
-            use_bias=False,
-            variable_dtype=context.variable_dtype,
-            name="reward_head"
-        )
-        # Squeeze out size 1 reward dimension
-        squeezed_shape = mtf.Shape([d for d in rewards.shape.dims if d.name != "reward"])
-        rewards = mtf.reshape(rewards, squeezed_shape, name="squeeze_reward_dim")
-        # Keep reward only at EOS positions
-        targets_length_dim = get_dims_by_name(
-            tensor=context.sequence_id,
-            dim_name="targets_length"
-        )[0]
-        shifted_segmentation = mtf.shift(
-            context.sequence_id,
-            offset=-1,
-            dim=targets_length_dim,
-            wrap=False
-        )
-        is_eos = mtf.not_equal(context.sequence_id, shifted_segmentation)
-        eos_rewards_long = mtf.cast(is_eos, dtype=rewards.dtype) * rewards
-        eos_rewards = mtf.reduce_sum(
-            eos_rewards_long,
-            reduced_dim=targets_length_dim
-        )
-        ans_pair_dims = get_dims_by_name(rewards, "ans_pair")
-        if ans_pair_dims and context.losses is not None:
+        del targets
+        if context.mode in [tf.estimator.ModeKeys.TRAIN, tf.estimator.ModeKeys.EVAL]:
+            # Input sequences (answers) should come stacked in pairs
+            assert context.length_dim.size == SEQUENCE_LENGTH["targets"] * 2
+            def _get_target_pair_element(x, i):
+                return mtf.slice(
+                    x,
+                    begin=SEQUENCE_LENGTH["targets"] * i,
+                    size=SEQUENCE_LENGTH["targets"],
+                    slice_dim_name="length",
+                    name="split_answer_pair"
+                )
+            # Slice answer pairs and solve reward for each separately
+            reward_pairs = []
+            for i in range(2):
+                inputs_i = _get_target_pair_element(inputs, i)
+                context_i = deepcopy(context)
+                context_i.mode = tf.estimator.ModeKeys.PREDICT
+                context_i.losses = None
+                context_i.length_dim = get_dims_by_name(inputs_i, "length")[0]
+                context_i.sequence_id = _get_target_pair_element(context.sequence_id, i)
+                context_i.subsequence_id = _get_target_pair_element(context.subsequence_id, i)
+                context_i.position = _get_target_pair_element(context.position, i)
+                reward_pairs.append(self._call_internal(context_i, inputs_i, None))
+            # Compute loss and return
+            reward_pairs = mtf.stack(reward_pairs, dim_name="ans_pair", name="stack_reward")
             loss = comparative_paired_rewards_loss(
-                paired_rewards=eos_rewards,
-                ans_pair_dim=ans_pair_dims[0],
-                batch_dim=get_dims_by_name(rewards, "batch")[0]
+                reward_pairs,
+                ans_pair_dim=get_dims_by_name(reward_pairs, "ans_pair")[0],
+                batch_dim=get_dims_by_name(reward_pairs, "batch")[0]
             )
-            context.losses.append(loss)
-        return eos_rewards
+            if context.losses:
+                context.losses.append(loss)
+            else:
+                context.losses = [loss]
+            return reward_pairs
+        elif context.mode == tf.estimator.ModeKeys.PREDICT:
+            assert context.length_dim.size == SEQUENCE_LENGTH["targets"]
+            mesh = inputs.mesh
+            if self.ensemble_dim and self.ensemble_dim not in inputs.shape.dims:
+                # Training an ensemble where all models are trained on the same examples.
+                inputs = mtf.broadcast(inputs, [self.ensemble_dim] + inputs.shape.dims)
+                if targets:
+                    targets = mtf.broadcast(
+                        targets, [self.ensemble_dim] + targets.shape.dims)
+            if "embedding" in context.shared_params:
+                vocab_embedding = context.shared_params["embedding"]
+            else:
+                vocab_embedding = get_vocab_embedding_cls()(
+                    mesh,
+                    self.input_vocab_dim,
+                    self.model_dim,
+                    context.variable_dtype,
+                    name="embedding",
+                    ensemble_dim=self.ensemble_dim)
+            x = vocab_embedding.ids_to_embedding(inputs)
+            if self.positional_embedding:
+                if "positional_embedding" in context.shared_params:
+                    pos_emb_var = context.shared_params["positional_embedding"]
+                else:
+                    pos_emb_var = mtf.layers.embedding_weights(
+                        mesh, self.max_length_dim, self.model_dim, context.variable_dtype,
+                        "positional_embedding", ensemble_dim=self.ensemble_dim)
+                if (context.length_dim is not None and
+                    context.length_dim.size > self.max_length_dim.size):
+                    message = (
+                        "Length dimenison exceeds size of positional embedding table. "
+                        "length_dim.size > max_length_dim.size %s vs %s."
+                        % (context.length_dim, self.max_length_dim))
+                    if context.position_is_default:
+                        # Definitely getting overflow in this case.
+                        raise ValueError(message)
+                    else:
+                        tf.logging.warning(
+                            message +
+                            " This may be OK if there are several shorter sequences packed "
+                            "together.  Otherwise, the later positions will get zeros.")
+                if context.position_is_default:
+                    pos_emb = mtf.rename_dimension(
+                        mtf.slice(pos_emb_var, 0, context.length_dim.size,
+                                self.max_length_dim.name),
+                        self.max_length_dim.name, context.length_dim.name)
+                else:
+                    pos_emb = mtf.gather(
+                        pos_emb_var, context.position, self.max_length_dim,
+                        output_shape=x.shape)
+                x += pos_emb
+            x = self.layer_stack.call(context, x)                                                                                                                                                                                                                                                                                
+            rewards = mtf.layers.dense(
+                x,
+                new_dims=self._reward_dim,
+                reduced_dims=get_dims_by_name(x, "d_model"),
+                use_bias=False,
+                variable_dtype=context.variable_dtype,
+                name="reward_head"
+            )
+            # Squeeze out size 1 reward dimension
+            squeezed_shape = mtf.Shape([d for d in rewards.shape.dims if d.name != "reward"])
+            rewards = mtf.reshape(rewards, squeezed_shape, name="squeeze_reward_dim")
+            # Keep reward only at EOS positions
+            length_dim = get_dims_by_name(
+                tensor=context.sequence_id,
+                dim_name="length"
+            )[0]
+            shifted_segmentation = mtf.shift(
+                context.sequence_id,
+                offset=-1,
+                dim=length_dim,
+                wrap=False
+            )
+            is_eos = mtf.not_equal(context.sequence_id, shifted_segmentation)
+            eos_rewards_long = mtf.cast(is_eos, dtype=rewards.dtype) * rewards
+            eos_rewards = mtf.reduce_sum(
+                eos_rewards_long,
+                reduced_dim=length_dim
+            )
+            return eos_rewards
+        else:
+            raise ValueError(f"Unrecognized mode: {context.mode}")
 
 @gin.configurable
 def make_reward_bitransformer(
