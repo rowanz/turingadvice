57bf7301484a4234a3529f3bc812ec15171d3270
JFCeron <juanfceron@gmail.com>
2021-07-29 16:58:17 -0500

    Define script flags within main

--- reward/comparative/data/ops.py
+++ reward/comparative/data/ops.py
@@ -1,11 +1,24 @@
+import os
 from functools import partial
 
 import tensorflow as tf
 import mesh_tensorflow.transformer.dataset as transformer_dataset
 
-from reward.comparative.data.tsvs_to_tfrecords import \
-    SEQUENCE_LENGTH, TFRECORDS_PATH as LOCAL_TFRECORDS_PATH
-
+SELFTEXT_DESIRED_LEN = 1250
+SEQUENCE_LENGTH = {"inputs": 1280, "targets": 512}
+_MONTHS = [
+    'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
+    'September', 'October', 'November', 'December'
+]
+# Data file constants
+SPLITS = ["train", "val", "test"]
+TSV_PATH = os.path.join(os.path.dirname(__file__), "{split}_str.tsv")
+TSV_COLNAMES = ["inputs", "targets1", "targets2"]
+LOCAL_TFRECORDS_PATH = os.path.join(
+    os.path.dirname(__file__),
+    "{dataset_id}",
+    "{split}.tfrecords"
+)
 GCS_TFRECORDS_PATH = "gs://seri2021-advice/turingadvice/reward/comparative/data/{split}.tfrecords"
 
 def get_dataset(split, from_local=False):
@@ -100,3 +113,47 @@ def eval_dataset_fn(sequence_length, vocabulary, dataset_split):
         None,
         [ranking_accuracy]
     )
+
+from datetime import datetime
+from data.to_tfrecord_t5 import encoder, _trim_to_desired_length, _fix_reddit_text
+
+def preprocess(
+    question: dict, answer: str, vocabulary,
+    max_selftext_len: int = SELFTEXT_DESIRED_LEN
+    ):
+    """
+    Args:
+    question: dict
+        Dictionary with keys "subreddit", "created_utc", "title", "selftext"
+    answer: str
+        Answer to the question
+    vocabulary: vocabulary.Vocabulary
+        Str-to-int tokenizer
+    max_selftext_len: int
+        Max question selftext character length
+    """
+    dt_date = datetime.utcfromtimestamp(question["created_utc"])
+    str_date = \
+        _MONTHS[dt_date.month - 1] + " {}, {}".format(dt_date.day, dt_date.year)
+    str_question = "Subreddit: {} Date: {} Title: {} Selftext: {}".format(
+        _fix_reddit_text(question["subreddit"]),
+        _fix_reddit_text(str_date),
+        _fix_reddit_text(question["title"]),
+        _fix_reddit_text(_trim_to_desired_length(
+            encoder,
+            question["selftext"],
+            desired_len=max_selftext_len
+        ))
+    )
+    str_answer = _fix_reddit_text(answer)
+    tf_question = tf.cast(vocabulary.encode_tf(str_question), tf.int64)
+    tf_question = tf.pad(
+        tf_question,
+        paddings=[[0, SEQUENCE_LENGTH["inputs"] - tf_question.shape[0]]]
+    )
+    tf_answer = tf.cast(vocabulary.encode_tf(str_answer), tf.int64)
+    tf_answer = tf.pad(
+        tf_answer,
+        paddings=[[0, SEQUENCE_LENGTH["targets"] - tf_answer.shape[0]]]
+    )
+    return {"inputs": tf_question, "targets": tf_answer}
